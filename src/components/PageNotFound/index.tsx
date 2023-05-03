ed9eb3c10bcab87321a9241d8866e06900678c5f
sepideh-sharifani <sepideh.sharifani@gmail.com>
2023-05-03 13:42:09 +0330

    page not found created

--- src/components/PageNotFound/index.tsx
+++ src/components/PageNotFound/index.tsx
@@ -4,7 +4,10 @@ type Props = {}
 
 function PageNotFound({ }: Props) {
     return (
-        <div>PageNotFound</div>
+        <div className='flex flex-col items-center justify-center bg-homeBg h-full bg-cover'>
+            <span className='text-7xl'>404</span>
+            <span className='text-5xl'>Page Not Found</span>
+        </div>
     )
 }
 
